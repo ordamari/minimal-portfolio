3102a6424dacd11a6145a1ce49715b9466fec4e0
ordamari <ordamari971@gmail.com>
2024-06-15 18:58:44 +0300

    Add company logos to Colleagues component

--- components/colleagues/index.tsx
+++ components/colleagues/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import { InfiniteMovingCards } from '../ui/infinite-moving-cards'
 import testimonials from '@/constants/testimonials.constant'
+import companies from '@/constants/companies.constant'
 
 const Colleagues = () => {
   return (
@@ -10,7 +12,17 @@ const Colleagues = () => {
       </h1>
       <div className='flex flex-col items-center mt-10'>
         <InfiniteMovingCards items={testimonials} speed='slow' />
-        <div className=''></div>
+        <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 mt-10'>
+          {companies.map(company => {
+            const { id, name, img, nameImg } = company
+            return (
+              <div key={id} className='flex md:max-w-16 max-w-32 gap-2'>
+                <img src={img} alt={name} className='md:w-10 w-5' />
+                <img src={nameImg} alt={name} className='md:w-24 w-20' />
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
