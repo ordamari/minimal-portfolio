13403d877ab284721af06728664bc2ab940ecbd1
ordamari <ordamari971@gmail.com>
2024-06-21 11:33:51 +0300

    Remove Sentry example API and page, update Next.js and Sentry configurations

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,19 +1,7 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://8ac9745a636d07b0206ab3783d06d99b@o4507436210257920.ingest.de.sentry.io/4507436221857872",
-
-  // Adjust this value in production, or use tracesSampler for greater control
+  dsn: 'https://8ac9745a636d07b0206ab3783d06d99b@o4507436210257920.ingest.de.sentry.io/4507436221857872',
   tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
-});
+  debug: false
+})
